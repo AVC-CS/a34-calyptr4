f375813295aa513ea199c44a5d8dd861a7b86159
calyptr4 <alessandrabaires@gmail.com>
2026-03-12 18:12:33 -0700

    a34

--- main.cpp
+++ main.cpp
@@ -8,9 +8,15 @@ int main()
   double avg;
 
   // TODO: Read three months and their rainfall values from standard input
-
+cout << "Type the first month its rainfall. \n";
+cin >> month1 >> rainfall1;
+cout << "Type the second month its rainfall. \n";
+cin >> month2 >> rainfall2;
+cout << "Type the third month its rainfall. \n";
+  cin >> month3 >> rainfall3;
   // TODO: Calculate the average rainfall
-
+avg = (rainfall1 + rainfall2 + rainfall3) / 3.0;
   cout << fixed << setprecision(2);
   // TODO: Print the result in the required format
+  cout << "The average rainfall for " << month1 << ", " << month2 << ", and " << month3 << " is " << avg << endl;
 }
